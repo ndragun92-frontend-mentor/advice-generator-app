1c40f17b797c8c481e25984a1571925ee2a49a5a
ndragun92 <nemanjadragun92@gmail.com>
2023-02-20 11:34:26 +0100

    style: responsive

--- pages/index.vue
+++ pages/index.vue
@@ -1,20 +1,27 @@
 <template>
-  <div class="h-full flex items-center justify-center">
+  <main class="h-full flex items-center justify-center p-[16px] sm:p-0">
     <div
-      class="bg-neutral-darkGrayishBlue max-w-[540px] pt-[46px] pb-[72px] px-12 text-center rounded-xl relative"
+      class="bg-neutral-darkGrayishBlue max-w-[540px] pt-[39px] sm:pt-[46px] pb-[64px] sm:pb-[72px] px-6 sm:px-12 text-center rounded-xl relative"
     >
-      <h1 class="text-primary-neonGreen uppercase tracking-[3.5px] text-[14px]">
+      <h1
+        class="text-primary-neonGreen uppercase tracking-[2.8px] sm:tracking-[3.5px] text-[12px] sm:text-[14px]"
+      >
         Advice <span>#117</span>
       </h1>
       <blockquote
-        class="mt-[22px] text-[28px] leading-[38px] tracking-[-0.3px]"
+        class="mt-[22px] text-[24px] sm:text-[28px] leading-[33px] sm:leading-[38px] tracking-[-0.3px]"
       >
         “It is easy to sit up and take notice, what's difficult is getting up
         and taking action.”
       </blockquote>
-      <div class="mt-10">
+      <div class="mt-6 sm:mt-10">
         <img
-          class="block mx-auto"
+          class="block sm:hidden mx-auto"
+          src="/images/pattern-divider-mobile.svg"
+          alt="Mobile divider"
+        />
+        <img
+          class="hidden sm:block mx-auto"
           src="/images/pattern-divider-desktop.svg"
           alt="Desktop divider"
         />
@@ -31,7 +38,7 @@
         </button>
       </div>
     </div>
-  </div>
+  </main>
 </template>
 
 <script lang="ts">
